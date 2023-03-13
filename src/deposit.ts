9095df11aeebe3f0e5b619f8bca0d32e6d78ea1d
inphi <mlaw2501@gmail.com>
2023-03-13 16:04:20 -0400

    stop deposits if balance is low

--- src/deposit.ts
+++ src/deposit.ts
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers';
 import { BaseServiceV2, validators } from '@eth-optimism/common-ts';
 import { CrossChainMessenger, MessageStatus } from '@eth-optimism/sdk';
+import { sleep } from '@eth-optimism/core-utils'
 import { getCrossChainMessenger } from './config';
 
 type DepositServiceState = {
@@ -18,10 +19,7 @@ export class DepositService extends BaseServiceV2<DepositServiceOptions, {}, Dep
             name: 'chaos-deposit-actor',
             version: "0.0.1",
             loop: true,
-            options: {
-                loopIntervalMs: 120_000,
-                ...options,
-            },
+            options: options,
             optionsSpec: {
                 amount: {
                     validator: validators.str,
@@ -30,6 +28,7 @@ export class DepositService extends BaseServiceV2<DepositServiceOptions, {}, Dep
             },
             metricsSpec: {},
         })
+        this.loopIntervalMs = 120_000
     }
 
     protected async init(): Promise<void> {
@@ -41,11 +40,18 @@ export class DepositService extends BaseServiceV2<DepositServiceOptions, {}, Dep
         const logger = this.logger.child({ component: 'DepositActor' })
         logger.info("Deposit ETH")
 
+        const balance = await this.state.crossChainMessenger.l1Signer.getBalance()
+        const teneth = ethers.utils.parseEther("10.0")
+        if (balance.lt(teneth)) {
+            logger.warn(`L1 wallet is too low. Currently has ${balance} wei`)
+            return
+        }
+
         const start = new Date().getUTCSeconds()
         const now = () => { return new Date().getUTCSeconds() }
 
         const response = await this.state.crossChainMessenger.depositETH(this.state.amount)
-        logger.info(`Transaction hash(on L1): ${response.hash}`)
+        logger.info(`Transaction hash(on L1): ${response.hash} `)
         await response.wait()
         logger.info("Waiting for status to change to RELAYED")
         logger.info(`Time so far ${(now() - start) / 1000} seconds`)
